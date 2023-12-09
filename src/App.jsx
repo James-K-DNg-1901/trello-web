5e83e74a3791954e8ae4134e400ba7785a370ed4
jamsekevinson <duytt2k@gmail.com>
2023-12-09 15:28:03 +0800

    add base line

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
-import * as React from 'react'
 import Button from '@mui/material/Button'
+import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
+import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
+import HomeIcon from '@mui/icons-material/Home'
+import { pink } from '@mui/material/colors'
 
 export default function App() {
   return (
     <>
-    `<Button variant="contained">Hello world</Button>
+      <Button variant="contained">Hello world</Button>
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+      <br />
+      <AccessAlarmIcon />
+      <ThreeDRotation />
+      <HomeIcon />
+      <HomeIcon color="primary" />
+      <HomeIcon color="secondary" />
+      <HomeIcon color="success" />
+      <HomeIcon color="action" />
+      <HomeIcon color="disabled" />
+      <HomeIcon sx={{ color: pink[100] }} />
     </>
   )
 }
